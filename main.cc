9431708d973c8b8d631c86aa4483c0b8f707c1a3
martinbuchta <tamabuch@gmail.com>
2019-11-11 19:48:43 +0100

    Forwarding advertise messages back to client

--- main.cc
+++ main.cc
@@ -11,6 +11,11 @@
 #include <netinet/in.h>
 #include <ifaddrs.h>
 #include <cstdint>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 
 using namespace std;
 
@@ -70,8 +75,6 @@ char *getFirstDevice()
         exit(1);
     }
 
-    dev = "enp4s0f1";
-
     return dev;
 }
 
@@ -108,8 +111,8 @@ struct in6_addr getIp6OfInterface()
             }
 
             if (strcmp(interface, ifa_tmp->ifa_name) == 0  && ifa_tmp->ifa_addr->sa_family == AF_INET6) {
-                printf("\nname = %s\n", ifa_tmp->ifa_name);
-                printf("addr = %s\n", addr);
+                //printf("\nname = %s\n", ifa_tmp->ifa_name);
+                //printf("addr = %s\n", addr);
                 return in6->sin6_addr;
             }
         }
@@ -126,7 +129,7 @@ void sendUdpForward(char *data, size_t length)
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } else {
-        printf("Socket created\n");
+        //printf("Socket created\n");
     } 
 
     struct sockaddr_in6     servaddr; 
@@ -146,14 +149,98 @@ void sendUdpForward(char *data, size_t length)
         perror("UDP err");
         exit(1);
     }
+
+    printf("RELY FORWARD sent\n");
 }
+
+void sendUdpReply(uint8_t *data, size_t length, struct in6_addr clientAddr, struct in6_addr linkAddr)
+{
+    int sockfd;
+    if ( (sockfd = socket(AF_INET6, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("REPLY Socket created\n");
+    }
+
+    // bind to interface
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "vboxnet0");
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
+        fprintf(stderr, "Can't bind socket to interface\n");
+        exit(13);
+    }
+
+
+    // bind source ip
+    struct sockaddr_in6 localaddr;
+    localaddr.sin6_family = AF_INET6;
+    localaddr.sin6_addr = linkAddr;
+    localaddr.sin6_port = 467;
+    bind(sockfd, (struct sockaddr *) &localaddr, sizeof(localaddr));
+
+    struct sockaddr_in6     servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_port = htons(546);
+    servaddr.sin6_addr = clientAddr;
+
+    if( sendto(sockfd, (const char *)data, length,
+               MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+               sizeof(servaddr)) <0 ) {
+        /* buffers aren't available locally at the moment,
+        * try again.
+        */
+        perror("UDP err");
+        exit(1);
+    }
+
+    printf("RELY REPLY sent\n");
+}
+
+void callbackServer(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
+{
+    struct ether_header ethernet_header;
+    memcpy(&ethernet_header, packet, sizeof(struct ether_header));
+
+    struct ipv6_header ip_header;
+    memcpy(&ip_header, packet + sizeof(struct ether_header), sizeof(struct ipv6_header));
+
+    if (ip_header.next_header != 17) { // TODO
+        printf("Oh, this should never happen :( \n Next header should be UDP, but isn't.\n");
+    }
+
+    const struct udphdr *udp_header;
+    udp_header = (struct udphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ipv6_header));
+
+    const u_char *dhcpMshType = packet + sizeof(struct ether_header) + sizeof(struct ipv6_header) + sizeof(struct udphdr);
+    const uint8_t msgType = *dhcpMshType;
+
+    if (msgType == 13) {
+        printf("I have rely reply (13) message\n");
+
+        struct dhcpv6_relay_server_message *msg = (struct dhcpv6_relay_server_message *) dhcpMshType;
+        struct option *opt = (struct option *) &(msg->options);
+
+        if (opt->option_code[1] != 9) { // relay msg
+            fprintf(stderr, "This should never happen!\n"); // TODO
+            exit(12);
+        }
+
+        sendUdpReply(opt->option_data, opt->option_length[1], msg->peer_addr, msg->link_addr);
+    }
+}
+
 
 void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
 {
     useless = NULL;
     static int count = 1;
 
-    printf("\nPacket number [%d], length of this packet is: %d\n", count++, pkthdr->len);
+    //printf("\nPacket number [%d], length of this packet is: %d\n", count++, pkthdr->len);
 
     /**
      * ethernet header
@@ -162,18 +249,18 @@ void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *p
     struct ether_header ethernet_header;
     memcpy(&ethernet_header, packet, sizeof(struct ether_header));
 
-    printf("--------- Ethernet ---------\n");
+    /*printf("--------- Ethernet ---------\n");
     printf("Source: ");
     printf("%02x:%02x:%02x:%02x:%02x:%02x\n", ethernet_header.ether_shost[0], ethernet_header.ether_shost[1],
            ethernet_header.ether_shost[2], ethernet_header.ether_shost[3], ethernet_header.ether_shost[4], ethernet_header.ether_shost[5]);
     printf("Dest: ");
     printf("%02x:%02x:%02x:%02x:%02x:%02x\n", ethernet_header.ether_dhost[0], ethernet_header.ether_dhost[1],
-           ethernet_header.ether_dhost[2], ethernet_header.ether_dhost[3], ethernet_header.ether_dhost[4], ethernet_header.ether_dhost[5]);
+           ethernet_header.ether_dhost[2], ethernet_header.ether_dhost[3], ethernet_header.ether_dhost[4], ethernet_header.ether_dhost[5]);*/
 
     struct ipv6_header ip_header;
     memcpy(&ip_header, packet + sizeof(struct ether_header), sizeof(struct ipv6_header));
-    printf("--------- IPv6 ---------\n");
-    printf("Next header: %u\n", ip_header.next_header);
+    /*printf("--------- IPv6 ---------\n");
+    printf("Next header: %u\n", ip_header.next_header);*/
 
     if (ip_header.next_header != 17) { // TODO
         printf("Oh, this should never happen :( \n Next header should be UDP, but isn't.\n");
@@ -181,26 +268,26 @@ void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *p
 
     const struct udphdr *udp_header;
     udp_header = (struct udphdr *) (packet + sizeof(struct ether_header) + sizeof(struct ipv6_header));
-    printf("--------- UDP ---------\n");
+    /*printf("--------- UDP ---------\n");
     printf("Source: %d\n", ntohs(udp_header->uh_sport));
     printf("Dest: %d\n", ntohs(udp_header->uh_dport));
     printf("Length: %d\n", ntohs(udp_header->uh_ulen));
 
-    printf("---- zkousim stesti DHCP ----");
+    printf("---- zkousim stesti DHCP ----");*/
     const u_char *dhcpMshType = packet + sizeof(struct ether_header) + sizeof(struct ipv6_header) + sizeof(struct udphdr);
     const uint8_t msgType = *dhcpMshType;
     printf("Msg-Type: %d\n", msgType);
 
     if (msgType == 1) {
-        printf("I have a SOLICIT (1) message.\n");
-        printf("For relay rofward message alloc %d bytes\n", 34 + ntohs(udp_header->uh_ulen) - 8 + 12);
+        /*printf("I have a SOLICIT (1) message.\n");
+        printf("For relay rofward message alloc %d bytes\n", 34 + ntohs(udp_header->uh_ulen) - 8 + 12);*/
 
         // create relay forward message
         
         struct dhcpv6_relay_server_message *msg = (struct dhcpv6_relay_server_message *) malloc(
             (34 + ntohs(udp_header->uh_ulen) - 8) + (12) + 100); // 100 bytu alokuju navic, abych se pojistil, ze omylem neprepisu data od klihovny c pro spravu pameti
 
-        printf("malloc ok\n");
+        //printf("malloc ok\n");
         msg->msgType = 12;
         msg->hopCount = 0;
         msg->link_addr = getIp6OfInterface();
@@ -239,33 +326,58 @@ int main()
     bpf_u_int32 pNet;  /* ip address*/
     cout << "Hello world!\n";
 
-    interface = getFirstDevice();
-    printf("Device: %s\n", interface);
+    if (fork() == 0) {
+        // sniffing interface that comunicates with server
+        interface = "enp4s0f1";
+        printf("Device: %s\n", interface);
 
-    pcap_lookupnet(interface, &pNet, &pMask, errbuf);
+        pcap_lookupnet(interface, &pNet, &pMask, errbuf);
 
-    pcap_t *descr = pcap_open_live(interface, BUFSIZ, 1, -1, errbuf);
-    if (descr == NULL)
-    {
-        printf("pcap_open_live() failed due to [%s]\n", errbuf);
-        return -1;
+        pcap_t *descr = pcap_open_live(interface, BUFSIZ, 1, -1, errbuf);
+        if (descr == NULL) {
+            printf("pcap_open_live() failed due to [%s]\n", errbuf);
+            return -1;
+        }
+
+        // Compile the filter expression
+        if (pcap_compile(descr, &fp, "port 547", 0, pNet) == -1) {
+            printf("\npcap_compile() failed\n");
+            return -1;
+        }
+
+        // Set the filter compiled above
+        if (pcap_setfilter(descr, &fp) == -1) {
+            printf("\npcap_setfilter() failed\n");
+            exit(1);
+        }
+
+        pcap_loop(descr, 1500, callbackServer, NULL);
+    } else {
+        interface = "vboxnet0";
+        printf("Device: %s\n", interface);
+
+        pcap_lookupnet(interface, &pNet, &pMask, errbuf);
+
+        pcap_t *descr = pcap_open_live(interface, BUFSIZ, 1, -1, errbuf);
+        if (descr == NULL) {
+            printf("pcap_open_live() failed due to [%s]\n", errbuf);
+            return -1;
+        }
+
+        // Compile the filter expression
+        if (pcap_compile(descr, &fp, "port 547", 0, pNet) == -1) {
+            printf("\npcap_compile() failed\n");
+            return -1;
+        }
+
+        // Set the filter compiled above
+        if (pcap_setfilter(descr, &fp) == -1) {
+            printf("\npcap_setfilter() failed\n");
+            exit(1);
+        }
+
+        pcap_loop(descr, 1500, callback, NULL);
     }
-
-    // Compile the filter expression
-    if (pcap_compile(descr, &fp, "port 546 or port 547", 0, pNet) == -1)
-    {
-        printf("\npcap_compile() failed\n");
-        return -1;
-    }
-
-    // Set the filter compiled above
-    if (pcap_setfilter(descr, &fp) == -1)
-    {
-        printf("\npcap_setfilter() failed\n");
-        exit(1);
-    }
-
-    pcap_loop(descr, 1500, callback, NULL);
 
     //pcap_loop();
 
