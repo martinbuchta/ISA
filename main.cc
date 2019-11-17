a89a54726fb72c808c88418ba1701011d47bb6de
martinbuchta <tamabuch@gmail.com>
2019-11-17 15:21:53 +0100

    Hton and ntoh functions

--- main.cc
+++ main.cc
@@ -70,16 +70,16 @@ struct __attribute__((__packed__)) dhcpv6_relay_server_message
 
 struct __attribute__((__packed__)) option
 {
-    uint8_t option_code[2];
-    uint8_t option_length[2];
+    uint16_t option_code;
+    uint16_t option_length;
     uint8_t option_data[];
 };
 
 struct __attribute__((__packed__)) mac_option
 {
-    uint8_t option_code[2];
-    uint8_t option_length[2];
-    uint8_t link_layer_type[2];
+    uint16_t option_code;
+    uint16_t option_length;
+    uint16_t link_layer_type;
     uint8_t link_layer_addr[];
 };
 
@@ -259,19 +259,19 @@ void callbackServer(const u_char *packet, unsigned int packetLength)
         while (packetLength > 8 + 34 + move) {
             opt = (struct option *) ((char *) &(msg->options) + move);
 
-            if (opt->option_code[1] == 9) {
+            if (ntohs(opt->option_code) == 9) {
                 // TODO check, if the address is in the map
                 msgPtr = opt->option_data;
-                msgSize = opt->option_length[1];
+                msgSize = ntohs(opt->option_length);
                 usedOptions++;
                 if (opt->option_data[0] == 7) {
                     struct option *replyOption = (struct option *) &(opt->option_data[4]);
 
-                    while (((char *) replyOption) < ((char *) opt) + opt->option_length[1]) {
-                        if (replyOption->option_code[1] == 3) {
+                    while (((char *) replyOption) < ((char *) opt) + ntohs(opt->option_length)) {
+                        if (ntohs(replyOption->option_code) == 3) {
                             struct option *addrOption = (struct option *) &(replyOption->option_data[12]);
-                            while ((char *) addrOption < ((char *) replyOption) + replyOption->option_length[1]) {
-                                if (addrOption->option_code[1] == 5) {
+                            while ((char *) addrOption < ((char *) replyOption) + ntohs(replyOption->option_length)) {
+                                if (ntohs(addrOption->option_code) == 5) {
                                     char ipHumbanBuff[INET6_ADDRSTRLEN];
                                     char ipReceived[INET6_ADDRSTRLEN];
                                     inet_ntop(AF_INET6, &(msg->peer_addr), ipHumbanBuff, sizeof(ipHumbanBuff));
@@ -290,14 +290,14 @@ void callbackServer(const u_char *packet, unsigned int packetLength)
                                     break;
                                 } else {
                                     addrOption = (struct option *) (((char *) addrOption) +
-                                                                    addrOption->option_length[1] + 4);
+                                            ntohs(addrOption->option_length) + 4);
                                 }
                             }
                             break;
-                        } else if (replyOption->option_code[1] == 4) {
+                        } else if (ntohs(replyOption->option_code) == 4) {
                             struct option *addrOption = (struct option *) &(replyOption->option_data[4]);
-                            while ((char *) addrOption < ((char *) replyOption) + replyOption->option_length[1]) {
-                                if (addrOption->option_code[1] == 5) {
+                            while ((char *) addrOption < ((char *) replyOption) + ntohs(replyOption->option_length)) {
+                                if (ntohs(addrOption->option_code) == 5) {
                                     char ipHumbanBuff[INET6_ADDRSTRLEN];
                                     char ipReceived[INET6_ADDRSTRLEN];
                                     inet_ntop(AF_INET6, &(msg->peer_addr), ipHumbanBuff, sizeof(ipHumbanBuff));
@@ -316,14 +316,14 @@ void callbackServer(const u_char *packet, unsigned int packetLength)
                                     break;
                                 } else {
                                     addrOption = (struct option *) (((char *) addrOption) +
-                                                                    addrOption->option_length[1] + 4);
+                                            ntohs(addrOption->option_length) + 4);
                                 }
                             }
                             break;
-                        } else if (replyOption->option_code[1] == 25) {
+                        } else if (ntohs(replyOption->option_code) == 25) {
                             struct option *addrOption = (struct option *) &(replyOption->option_data[12]);
-                            while ((char *) addrOption < ((char *) replyOption) + replyOption->option_length[1]) {
-                                if (addrOption->option_code[1] == 26) {
+                            while ((char *) addrOption < ((char *) replyOption) + ntohs(replyOption->option_length)) {
+                                if (ntohs(addrOption->option_code) == 26) {
                                     uint8_t prefix = (uint8_t) addrOption->option_data[8];
                                     char prefixBuff[10];
                                     sprintf(prefixBuff, "%d", prefix);
@@ -346,25 +346,25 @@ void callbackServer(const u_char *packet, unsigned int packetLength)
                                     break;
                                 } else {
                                     addrOption = (struct option *) (((char *) addrOption) +
-                                                                    addrOption->option_length[1] + 4);
+                                            ntohs(addrOption->option_length) + 4);
                                 }
                             }
                             break;
                         } else {
-                            replyOption = (struct option *) (((char *) replyOption) + replyOption->option_length[1]);
+                            replyOption = (struct option *) (((char *) replyOption) + ntohs(replyOption->option_length));
                         }
                     }
                 }
             }
 
-            if (opt->option_code[1] == 18) {
-                memcpy(interface, opt->option_data, opt->option_length[1]);
-                interface[opt->option_length[1]] = '\0';
+            if (ntohs(opt->option_code) == 18) {
+                memcpy(interface, opt->option_data, ntohs(opt->option_length));
+                interface[ntohs(opt->option_length)] = '\0';
 
                 usedOptions++;
             }
 
-            move += opt->option_length[1] + 4;
+            move += ntohs(opt->option_length) + 4;
         }
 
         if (usedOptions < 2) {
@@ -468,30 +468,26 @@ void callback(u_char *interface, const struct pcap_pkthdr *pkthdr, const u_char 
         msg->peer_addr = ip_header.src;
 
         struct option *relay_message_option = (struct option *) &(msg->options);
-        relay_message_option->option_code[0] = 0;
-        relay_message_option->option_code[1] = 9;
-        relay_message_option->option_length[0] = 0;
-        relay_message_option->option_length[1] = ntohs(udp_header->uh_ulen) - 8;
+        relay_message_option->option_code = htons(0);
+        relay_message_option->option_code = htons(9);
+        relay_message_option->option_length = htons(ntohs(udp_header->uh_ulen) - 8);
         memcpy(&(relay_message_option->option_data), dhcpMshType, ntohs(udp_header->uh_ulen) - 8);
 
         char *macOptionAddr = (char *) &(msg->options);
         macOptionAddr += ntohs(udp_header->uh_ulen) - 8 + 4;
         struct mac_option *macOption = (struct mac_option *) macOptionAddr;
-        macOption->option_code[0] = 0;
-        macOption->option_code[1] = 79;
-        macOption->option_length[0] = 0;
-        macOption->option_length[1] = 8;
-        macOption->link_layer_type[0] = 0;
-        macOption->link_layer_type[1] = 1;
+        macOption->option_code = htons(0);
+        macOption->option_code = htons(79);
+        macOption->option_length = htons(8);
+        macOption->link_layer_type = htons(1);
         memcpy(&(macOption->link_layer_addr), &(ethernet_header.ether_shost), 6);
 
         char *interfaceIdOptionAddr = (char *) &(msg->options);
         interfaceIdOptionAddr += ntohs(udp_header->uh_ulen) - 8 + 4 + 12;
         struct option *interfaceIdOption = (struct option *) interfaceIdOptionAddr;
-        interfaceIdOption->option_code[0] = 0;
-        interfaceIdOption->option_code[1] = 18;
-        interfaceIdOption->option_length[0] = 0;
-        interfaceIdOption->option_length[1] = strlen(currentInterface);
+        interfaceIdOption->option_code = htons(0);
+        interfaceIdOption->option_code = htons(18);
+        interfaceIdOption->option_length = htons(strlen(currentInterface));
         memcpy(&(interfaceIdOption->option_data), currentInterface, strlen(currentInterface));
 
         //printf("================ interface %s\n\n\n", currentInterface);
